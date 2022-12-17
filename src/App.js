7ada78be18b86139b011683fc98999eaf1a10b9a
Kağan Çubukçu <kagancancubukcu@gmail.com>
2022-12-17 10:29:11 +0300

    Update codes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import moment from "moment";
+import "moment/locale/tr";
+
 import { MainContext } from "./components/context";
 import Card from "./components/Card";
 import Dropdown from "./components/Dropdown";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 import Images from "./components/Images";
-import moment from "moment";
-import "moment/locale/tr";
-import axios from "axios";
 
 function App() {
   const [data, setData] = useState();
   const [location, setLocation] = useState("Ankara");
+
+  //API
   const API_KEY = process.env.REACT_APP_API_KEY;
   const url = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${location}&aqi=no&alerts=no&days=7&lang=tr`;
 
@@ -24,11 +27,10 @@ function App() {
 
   const searchLocation = async () => {
     try {
-      const response = await axios.get(url).then((response) => {
-        setData(response.data);
-      });
+      const { data } = await axios.get(url);
+      setData(data);
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   };
 
@@ -36,73 +38,40 @@ function App() {
 
   const time = momentAll.momentHour;
 
-  const dayCycle = `${
-    (time < "17:59" && "Morning") || (time > "18:00" && "Evening") || "None"
-  }`;
+  const dayCycle = time < "18:00" ? "Morning" : "Evening";
+
+  const conditions = {
+    Güneşli: "clear",
+    Açık: "clear",
+    Bulutlu: "cloudy",
+    "Parçalı Bulutlu": "cloudy",
+    "Çok bulutlu": "cloudy",
+    Puslu: "cloudy",
+    "Hafif yağmurlu": "rain",
+    "Ara ara orta kuvvetli yağmurlu": "rain",
+    "Orta kuvvetli yağmurlu": "rain",
+    "Ara ara şiddetli yağmurlu": "rain",
+    "Şiddetli yağmurlu": "rain",
+    "Şiddetli sağnak yağmur": "rain",
+    "Bölgesel düzensiz yağmur yağışlı": "rain",
+    "Bölgesel gök gürültülü düzensiz hafif yağmur": "rain",
+    "Hafif sağnak yağışlı": "rain",
+    "Bölgesel düzensiz gök gürültülü yağmurlu": "storm",
+    "Kar fırtınası": "snow",
+    "Düzensiz hafif karlı": "snow",
+    "Hafif karlı": "snow",
+    "Orta kuvvetli karlı": "snow",
+    "Yoğun kar yağışlı": "snow",
+    Kapalı: "overcast",
+    Sisli: "overcast",
+  };
 
   const coditionStyleDay = {
-    background:
-      condition === "Güneşli" || condition === "Açık"
-        ? `url(${Images.day.clear}) no-repeat center`
-        : condition === "Bulutlu" ||
-          condition === "Parçalı Bulutlu" ||
-          condition === "Çok bulutlu"
-        ? `url(${Images.day.cloudy}) no-repeat center `
-        : condition === "Hafif yağmurlu" ||
-          condition === "Ara ara orta kuvvetli yağmurlu" ||
-          condition === "Orta kuvvetli yağmurlu" ||
-          condition === "Ara ara şiddetli yağmurlu" ||
-          condition === "Şiddetli yağmurlu" ||
-          condition === "Şiddetli sağnak yağmur" ||
-          condition === "Bölgesel düzensiz yağmur yağışlı" ||
-          condition === "Bölgesel gök gürültülü düzensiz hafif yağmur" ||
-          condition === "Hafif sağnak yağışlı"
-        ? `url(${Images.day.rain}) no-repeat center`
-        : condition === "Bölgesel düzensiz gök gürültülü yağmurlu"
-        ? `url(${Images.day.storm}) no-repeat center`
-        : condition === "Kar fırtınası" ||
-          condition === "Düzensiz hafif karlı" ||
-          condition === "Hafif karlı" ||
-          condition === "Orta kuvvetli karlı" ||
-          condition === "Yoğun kar yağışlı"
-        ? `url(${Images.day.snow}) no-repeat center`
-        : condition === "Kapalı" ||
-          condition === "Çok bulutlu" ||
-          condition === "Sisli"
-        ? `url(${Images.day.overcast}) no-repeat center`
-        : null,
+    background: `url(${Images.day[conditions[condition]]}) no-repeat center`,
   };
+
   const coditionStyleNight = {
-    background:
-      condition === "Güneşli" || condition === "Açık"
-        ? `url(${Images.night.clear}) no-repeat center`
-        : condition === "Bulutlu" ||
-          condition === "Parçalı Bulutlu" ||
-          condition === "Çok bulutlu"
-        ? `url(${Images.night.cloudy}) no-repeat center `
-        : condition === "Hafif yağmurlu" ||
-          condition === "Ara ara orta kuvvetli yağmurlu" ||
-          condition === "Orta kuvvetli yağmurlu" ||
-          condition === "Ara ara şiddetli yağmurlu" ||
-          condition === "Şiddetli yağmurlu" ||
-          condition === "Şiddetli sağnak yağmur" ||
-          condition === "Bölgesel düzensiz yağmur yağışlı" ||
-          condition === "Bölgesel gök gürültülü düzensiz hafif yağmur" ||
-          condition === "Hafif sağnak yağışlı"
-        ? `url(${Images.night.rain}) no-repeat center`
-        : condition === "Bölgesel düzensiz gök gürültülü yağmurlu"
-        ? `url(${Images.night.storm}) no-repeat center`
-        : condition === "Kar fırtınası" ||
-          condition === "Düzensiz hafif karlı" ||
-          condition === "Hafif karlı" ||
-          condition === "Orta kuvvetli karlı" ||
-          condition === "Yoğun kar yağışlı"
-        ? `url(${Images.night.snow}) no-repeat center`
-        : condition === "Kapalı" ||
-          condition === "Çok bulutlu" ||
-          condition === "Sisli"
-        ? `url(${Images.night.cloudy}) no-repeat center`
-        : null,
+    background: `url(${Images.night[conditions[condition]]}) no-repeat center`,
   };
   const dataContext = {
     data,
@@ -115,11 +84,13 @@ function App() {
 
   return (
     <div
-      style={
-        dayCycle === "Morning"
-          ? { background: `${coditionStyleDay.background}`, height: "100vh" }
-          : { background: `${coditionStyleNight.background}`, height: "100vh" }
-      }
+      style={{
+        background:
+          dayCycle === "Morning"
+            ? coditionStyleDay.background
+            : coditionStyleNight.background,
+        height: "100vh",
+      }}
     >
       <MainContext.Provider value={dataContext}>
         <Dropdown />
